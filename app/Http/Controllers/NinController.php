1c42842f13a214272bdcccb05183f630b635bf83
C4P5LOCK <ji4seth@gmail.com>
2026-06-14 19:08:26 +0100

    Model and migration created

--- app/Http/Controllers/NinController.php
+++ app/Http/Controllers/NinController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class NinController extends Controller
 {
     //
+    public function showForm()
+        {
+            return view('nin.verify');
+        }
+
+        public function verify(Request $request)
+        {
+             $request->validate ([
+                'nin' => 'required'
+             ]);
+
+             $id = rand(1, 10);
+             $response = Http::withoutVerifying()->get("https://jsonplaceholder.typicode.com/users/{$id}");
+             $user = $response->json();
+
+             return view('nin.verify', compact('user'));
+        }
 }
--- resources/views/nin/verify.blade.php
+++ resources/views/nin/verify.blade.php
@@ -16,5 +16,22 @@
 
         <button type="submit">Verify NIN</button>
     </form>
+
+    @if(isset($user))
+        <hr>
+
+        <h3>Verification Result</h3>
+
+        <p><strong>Name:</strong> {{ $user['name'] }}</p>
+
+        <p><strong>Username:</strong> {{ $user['username'] }}</p>
+
+        <p><strong>Email:</strong> {{ $user['email'] }}</p>
+
+        <p><strong>Phone:</strong> {{ $user['phone'] }}</p>
+
+        <p><strong>Website:</strong> {{ $user['website'] }}</p>
+    @endif
+
 </body>
 </html>
